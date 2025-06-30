a48888212f22e109e893aca54df0f2ffd91214bb
lexuanhoang <82vodanh@gmail.com>
2025-06-30 15:18:47 +0700

    Push Product, User, Login

--- obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
+++ obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3db3e5f3262d3c450e23644be5156899f04158e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77041f49fe4f47375f48669da2e23e362ed1133d")]
 [assembly: System.Reflection.AssemblyProductAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
