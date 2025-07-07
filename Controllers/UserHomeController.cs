82644feee6f57bc49a7cbd09e5a77852aae34222
lehongphuc-12 <140739703+lehongphuc-12@users.noreply.github.com>
2025-07-07 10:04:41 +0700

    UserHome controller

---
+++ Controllers/UserHomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pet_spa_system1.ViewModel;
+
+namespace pet_spa_system1.Controllers
+{
+    public class UserHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userModel = new UserViewModel
+            {
+                UserName = "PhucLe",
+                AvatarUrl = "/images/default-avatar.jpg",
+                Email = "phucle@example.com",
+                PhoneNumber = "0123456789"
+            };
+
+            return View(userModel); // truyền model vào view
+        }
+    }
+}
