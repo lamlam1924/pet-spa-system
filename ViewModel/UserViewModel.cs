07f38a4b5e94cffcc4c25591c10cee8d1f11ed99
lehongphuc-12 <140739703+lehongphuc-12@users.noreply.github.com>
2025-07-07 09:59:31 +0700

    commit 1

---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace pet_spa_system1.ViewModel
+{
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
