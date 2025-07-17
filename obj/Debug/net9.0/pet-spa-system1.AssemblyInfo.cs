b37eeb32a3ef66d3b57734e668eb2cfc1624206a
lexuanhoang <82vodanh@gmail.com>
2025-07-18 00:48:29 +0700

    up order hictory cho user, chatbot, blog vẫn lỗi

--- obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
+++ obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13bf6f9f6f185b9e8ab0a635497711529f0189b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe5029ebfba0f80c041a89c570839523e7f6433")]
 [assembly: System.Reflection.AssemblyProductAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
