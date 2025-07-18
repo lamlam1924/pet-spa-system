4dd67f14ead5222d12c9833b7c7ff38f46720a6a
khaine2k4 <Khaihoanxcc@gmail.com>
2025-07-18 12:59:49 +0700

    up

--- obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
+++ obj/Debug/net9.0/pet-spa-system1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89d8defdda97652ab489632876152f906b6f35d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eb32dbac1e773d48984da699abb5eb8866e858")]
 [assembly: System.Reflection.AssemblyProductAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pet-spa-system1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
