44dd63eb95db66e6ec6e3078388cf0f443319e7f
lexuanhoang <82vodanh@gmail.com>
2025-07-20 14:31:48 +0700

    ok

--- Models/PetDataShopContext.cs
+++ Models/PetDataShopContext.cs
@@ -230,33 +230,6 @@ public partial class PetDataShopContext : DbContext
                 .HasConstraintName("FK__Blog_Imag__BlogI__503BEA1C");
         });
 
-        modelBuilder.Entity<BlogComment>(entity =>
-        {
-            entity.HasKey(e => e.CommentId);
-
-            entity.ToTable("Blog_Comments");
-
-            entity.Property(e => e.CommentId).HasColumnName("CommentID");
-            entity.Property(e => e.BlogId).HasColumnName("BlogID");
-            entity.Property(e => e.UserId).HasColumnName("UserID");
-            entity.Property(e => e.ParentCommentId).HasColumnName("ParentCommentID");
-            entity.Property(e => e.Status)
-                .HasMaxLength(20)
-                .HasDefaultValue("Pending");
-            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
-
-            entity.HasOne(d => d.Blog).WithMany(p => p.BlogComments)
-                .HasForeignKey(d => d.BlogId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entity.HasOne(d => d.User).WithMany()
-                .HasForeignKey(d => d.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entity.HasOne(d => d.ParentComment).WithMany(p => p.InverseParentComment)
-                .HasForeignKey(d => d.ParentCommentId)
-                .OnDelete(DeleteBehavior.NoAction);
-        });
 
         modelBuilder.Entity<BlogLike>(entity =>
         {
