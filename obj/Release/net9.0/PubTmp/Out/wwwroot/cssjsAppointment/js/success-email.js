b868b83cc8dae0907932085042e3ad2dec8266e9
lexuanhoang <82vodanh@gmail.com>
2025-08-09 07:55:26 +0700

    new version

--- obj/Release/net9.0/PubTmp/Out/wwwroot/cssjsAppointment/js/success-email.js
+++
@@ -1,3 +0,0 @@
-// success-email.js
-// Nếu bạn muốn thêm hiệu ứng hoặc xử lý JS cho email, thêm code tại đây.
-// Lưu ý: JS thường không chạy được trong hầu hết email client, chỉ dùng cho preview nội bộ hoặc trang web.
